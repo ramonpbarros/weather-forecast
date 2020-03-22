3b83dbeebd4069cac5920557596106653308f0d7
Sorrab <ramonpbarros@gmail.com>
2020-03-21 18:49:40 -0700

    added function to on click search event

--- script.js
+++ script.js
@@ -10,39 +10,102 @@ $(function () {
 
             // var icon;
             var apiKey = "a360aaabade6248b33523e2e88f37cff"
-            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&appid=" + apiKey;
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + apiKey;
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-                console.log(response);
-                
                 var cityTemp = response.main.temp;
-                cityTemp = (((cityTemp - 273.15) * 1.8) +32).toFixed(2);
                 var windSpeed = response.wind.speed;
+                var icon = response.weather[0].icon;
+                var cityLat = response.coord.lat;
+                var cityLon = response.coord.lon;
+                // var date = response.
+                console.log(response);
+
+                cityTemp = (((cityTemp - 273.15) * 1.8) + 32).toFixed(2);
                 windSpeed = (windSpeed * 2.237).toFixed(1);
-                icon = response.weather[0].icon;
-                // console.log(icon);
-                
+
+
                 $("#cityName").text(response.name);
                 $("#imgMainCard").attr("src", "http://openweathermap.org/img/wn/" + icon + ".png");
                 $("#temp").text("Temperature: " + cityTemp + " °F");
                 $("#humidity").text("Humidity: " + response.main.humidity + " %");
                 $("#wind").text("Wind Speed: " + windSpeed + " MPH");
 
-                // $(".city").text(response.name);
-                // $(".wind").text("Wind speed: " + response.wind.speed);
-                // $(".humidity").text("Humidity: " + response.main.humidity);
-                // $(".temp").text("Temperature: " + response.main.temp);
+                var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + cityLat + "&lon=" + cityLon;
+                $.ajax({
+                    url: uvURL,
+                    method: "GET"
+                }).then(function (result) {
+                    $("#uvIndexValue").text(" " + result.value);
+                });
+
+                var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + apiKey;
+                $.ajax({
+                    url: forecastURL,
+                    method: "GET"
+                }).then(function (forecastResponse) {
+                    var forecastIcon0 = forecastResponse.list[5].weather[0].icon;
+                    var forecastIcon1 = forecastResponse.list[13].weather[0].icon;
+                    var forecastIcon2 = forecastResponse.list[21].weather[0].icon;
+                    var forecastIcon3 = forecastResponse.list[29].weather[0].icon;
+                    var forecastIcon4 = forecastResponse.list[37].weather[0].icon;
+
+                    var forecastTemp0 = forecastResponse.list[5].main.temp;
+                    var forecastTemp1 = forecastResponse.list[13].main.temp;
+                    var forecastTemp2 = forecastResponse.list[21].main.temp;
+                    var forecastTemp3 = forecastResponse.list[29].main.temp;
+                    var forecastTemp4 = forecastResponse.list[37].main.temp;
+
+                    var forecastDate0 = (forecastResponse.list[5].dt_txt).split(" ")[0];
+                    var forecastDate1 = (forecastResponse.list[13].dt_txt).split(" ")[0];
+                    var forecastDate2 = (forecastResponse.list[21].dt_txt).split(" ")[0];
+                    var forecastDate3 = (forecastResponse.list[29].dt_txt).split(" ")[0];
+                    var forecastDate4 = (forecastResponse.list[37].dt_txt).split(" ")[0];
+
+                    convertTemp(forecastTemp0, $("#tempForecast0"));
+                    convertTemp(forecastTemp1, $("#tempForecast1"));
+                    convertTemp(forecastTemp2, $("#tempForecast2"));
+                    convertTemp(forecastTemp3, $("#tempForecast3"));
+                    convertTemp(forecastTemp4, $("#tempForecast4"));
+                    function convertTemp(variable, id) {
+                        variable = (((variable - 273.15) * 1.8) + 32).toFixed(2)
+                        id.text("Temp: " + variable + " °F")
+                    }
+
+                    convertDate(forecastDate0, $("#cityForecast0"));
+                    convertDate(forecastDate1, $("#cityForecast1"));
+                    convertDate(forecastDate2, $("#cityForecast2"));
+                    convertDate(forecastDate3, $("#cityForecast3"));
+                    convertDate(forecastDate4, $("#cityForecast4"));
+                    function convertDate(dateVar, dateId) {
+                        dateVar = dateVar.replace(/(....).(..).(..)/, "$2/$3/$1");
+                        dateId.text(dateVar);
+                    }
+
+                    $("#imgForecast0").attr("src", "http://openweathermap.org/img/wn/" + forecastIcon0 + ".png");
+                    $("#imgForecast1").attr("src", "http://openweathermap.org/img/wn/" + forecastIcon1 + ".png");
+                    $("#imgForecast2").attr("src", "http://openweathermap.org/img/wn/" + forecastIcon2 + ".png");
+                    $("#imgForecast3").attr("src", "http://openweathermap.org/img/wn/" + forecastIcon3 + ".png");
+                    $("#imgForecast4").attr("src", "http://openweathermap.org/img/wn/" + forecastIcon4 + ".png");
+
+                    $("#humForecast0").text("Humidity: " + forecastResponse.list[5].main.humidity + "%");
+                    $("#humForecast1").text("Humidity: " + forecastResponse.list[13].main.humidity + "%");
+                    $("#humForecast2").text("Humidity: " + forecastResponse.list[21].main.humidity + "%");
+                    $("#humForecast3").text("Humidity: " + forecastResponse.list[29].main.humidity + "%");
+                    $("#humForecast4").text("Humidity: " + forecastResponse.list[37].main.humidity + "%");
+
+
+                });
             });
-
-
+            
             var newLi = $("<li>");
             newLi.addClass("list-group-item list");
             newLi.text(searchInput);
             $("#unorderedList").append(newLi);
-
             $("#content").css("visibility", "visible")
+
         }
 
     });
